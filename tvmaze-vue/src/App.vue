08f5d5b031088b205aa61f64c41f3d0b517768a4
Rahimuddin Mohammad <mdrahiem@gmail.com>
2022-11-26 16:06:42 +0100

    vue3 carousel setup

--- tvmaze-vue/src/App.vue
+++ tvmaze-vue/src/App.vue
@@ -1,10 +1,13 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router';
-</script>
-
 <template>
   <header class="max-w-7xl mx-auto text-center">
     </header>
   <RouterView />
+  
 </template>
+
+
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+</script>
 
--- tvmaze-vue/src/views/Home.vue
+++ tvmaze-vue/src/views/Home.vue
@@ -1,12 +1,11 @@
-<script setup lang="ts">
-import Welcome from '../components/Welcome.vue'
-import SearchForm from '../components/SearchForm.vue'
-</script>
-
 <template>
-    
   <main>
     <Welcome />
-    <SearchForm />
+    <Carorsel />
   </main>
 </template>
+
+<script setup lang="ts">
+import Welcome from '../components/Welcome.vue'
+import Carorsel from '../components/Carorsel.vue';
+</script>
